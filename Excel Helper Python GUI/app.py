2d6cd6490f1b4d3c932b2accc1cda2fb0875a4c6
Sami Mnif <66852892+Samimnif@users.noreply.github.com>
2022-06-15 23:42:03 -0400

    Update app.py

--- Excel Helper Python GUI/app.py
+++ Excel Helper Python GUI/app.py
@@ -1,3 +1,4 @@
+from ast import Lambda
 from tkinter import *
 from tkinter import ttk
 import tkinter
@@ -5,7 +6,7 @@ import xlrd
 from tkinter.filedialog import askopenfilename
 import xlwt
 from xlwt import Workbook
-import os
+from time import sleep
 
 
 def show_sheets(loc):
@@ -17,13 +18,11 @@ def show_sheets(loc):
         n+=1
     return strin
 
-
-
 class app:
     def __init__(self, master):
         self.master = master
         self.master.title('Excel modifier')
-        self.master.geometry("500x500")
+        self.master.geometry("400x500")
         self.submitU = ''
         self.entry = ''
         self.entriesU = []
@@ -34,10 +33,12 @@ class app:
             i.destroy()
         self.frame1 = Frame(self.master, width=300, height=300)
         self.frame1.pack()
-        self.reg_txt = ttk.Label(self.frame1, text='Welcome to Excel modifier\nChoose between these options')
+        self.reg_txt = ttk.Label(self.frame1, text='  Welcome to Excel modifier\nChoose between these options\n\n')
         self.reg_txt.pack()
-        self.register_btn = ttk.Button(self.frame1, text="New File creation", command=self.get_sheet)
-        self.register_btn.pack()
+        self.get_sheet_btn = ttk.Button(self.frame1, text="New File creation", command=self.get_sheet)
+        self.get_sheet_btn.pack()
+        self.quit_btn = ttk.Button(self.frame1, text="Quit", command=self.master.destroy)
+        self.quit_btn.pack()
     
     def register(self):
         for i in self.master.winfo_children():
@@ -56,10 +57,13 @@ class app:
         self.filename = askopenfilename()
         
         self.frame3 = Frame(self.master, width=300, height=300)
-        self.frame3.pack()
-        self.reg_txt3 = ttk.Label(self.frame3, text=show_sheets(self.filename))
+        self.reg_txt3 = Text(self.frame3,font=("Helvetica", 10))
+        self.reg_txt3.insert(INSERT,show_sheets(self.filename))
         self.reg_txt3.pack()
-        self.entry= Entry(self.master, width= 40)
+        self.frame3.pack()
+        self.label = ttk.Label(self.frame3, text='Select sheet by enetring the number below:')
+        self.label.pack()
+        self.entry= Entry(self.frame3, width= 40)
         self.entry.focus_set()
         self.entry.pack()
         self.sheetnumber_btn = ttk.Button(self.frame3, text="submit number", command = self.get_col)
@@ -73,25 +77,29 @@ class app:
             i.destroy()
         self.frame4 = Frame(self.master, width=300, height=300)
         self.frame4.pack()
-        self.reg_txt4 = ttk.Label(self.frame4, text=show_cols(self.filename, self.submitU))
+        self.label2 = ttk.Label(self.frame4, text='For Reference')
+        self.label2.pack()
+        self.reg_txt4 = Text(self.frame4,font=("Helvetica", 10))
+        self.reg_txt4.insert(INSERT,show_cols(self.filename, self.submitU))
         self.reg_txt4.pack()
-        # self.entry= Entry(self.master, width= 40)
-        # self.entry.focus_set()
-        # self.entry.pack()
+        self.label3 = ttk.Label(self.frame4, text='Press Next to continue')
+        self.label3.pack()
         self.sheetnumber_btn = ttk.Button(self.frame4, text="Next", command = self.creating)
         self.sheetnumber_btn.pack()
         self.main_btn = ttk.Button(self.frame4, text="Back to Main", command=self.main)
         self.main_btn.pack()
     
     def creating(self):
-        for i in self.master.winfo_children():
-            i.destroy()
-        self.frame5 = Frame(self.master, width=300, height=300)
+        #for i in self.root.winfo_children():
+        #     i.destroy()
+        self.root = Tk()
+        self.root.title('Excel modifier')
+        self.root.geometry("400x500")
+        self.frame5 = Frame(self.root, width=300, height=300)
         self.frame5.pack()
-        print(self.submitU)
         self.reg_txt5 = ttk.Label(self.frame5, text='How many columns for new sheet')
         self.reg_txt5.pack()
-        self.entry= Entry(self.master, width= 40)
+        self.entry= Entry(self.frame5, width= 40)
         self.entry.focus_set()
         self.entry.pack()
         self.sheetnumber_btn = ttk.Button(self.frame5, text="submit number", command = self.new_sheet)
@@ -100,70 +108,88 @@ class app:
         self.main_btn.pack()
     def new_sheet(self):
         self.get_entry()
-        for i in self.master.winfo_children():
+        for i in self.root.winfo_children():
             i.destroy()
-        self.frame6 = Frame(self.master, width=300, height=300)
+        self.frame6 = Frame(self.root, width=300, height=300)
         self.frame6.grid(column = 1, row = 0)
-        self.reg_txt6 = ttk.Label(self.frame6, text='Enter column number replacing')
+        self.reg_txt6 = ttk.Label(self.frame6, text='Enter column number')
         self.reg_txt6.grid(column = 1, row = 0)
-        # self.entry= Entry(self.master, width= 40)
-        # self.entry.focus_set()
-        # self.entry.pack()
+        self.reg_txt7 = ttk.Label(self.frame6, text='Enter new column name')
+        self.reg_txt7.grid(column = 2, row = 0)
         self.entries = [Entry(self.frame6) for _ in range(int(self.submitU)*2)]
         n = 1
         nextLine= False
         for self.entry in self.entries:
             if nextLine:
                 self.entry.grid(column = 2, row = n)
-                #self.entry.pack()
                 nextLine =False
                 n += 1
             else:
                 self.reg_txt6 = ttk.Label(self.frame6, text=str(n))
                 self.reg_txt6.grid(column = 0, row = n)
-                #self.reg_txt6.pack()
                 self.entry.grid(column = 1, row = n)
-                #self.entry.pack()
                 nextLine = True
+        self.label4 = ttk.Label(self.frame6, text='New file Name:')
+        self.label4.grid(column = 2, row = n+1)
+        self.entryN= Entry(self.frame6)
+        self.entryN.focus_set()
+        self.entryN.grid(column = 2, row = n+2)
         self.sheetnumber_btn = ttk.Button(self.frame6, text="submit form", command = self.finalize)
-        self.sheetnumber_btn.grid(column = 1, row = n+1)
+        self.sheetnumber_btn.grid(column = 2, row = n+3)
         self.main_btn = ttk.Button(self.frame6, text="Back to Main", command=self.main)
-        self.main_btn.grid(column = 1, row = n+2)
+        self.main_btn.grid(column = 2, row = n+4)
     def getEnteries(self):
         for self.entry in self.entries:
             self.entriesU.append(self.entry.get())
-
+    
+    def getName(self):
+        self.submitU = self.entryN.get()
     def finalize(self):
         global externalS
         self.getEnteries()
+        self.getName()
         for i in self.master.winfo_children():
             i.destroy()
-        self.frame5 = Frame(self.master, width=300, height=300)
-        self.frame5.pack()
+        self.root.destroy()
+        self.frame8 = Frame(self.master, width=300, height=300)
+        self.frame8.pack()
         for self.entry in self.entriesU:
             print(self.entry)
-        self.reg_txt5 = ttk.Label(self.frame5, text='How many columns for new sheet')
+        self.reg_txt5 = ttk.Label(self.frame8, text='Saving ...')
         self.reg_txt5.pack()
+        self.pb1 = ttk.Progressbar(self.frame8, orient=HORIZONTAL, length=200, mode='determinate')
+        self.pb1.pack(expand=True)
         wb = Workbook()
         # add_sheet is used to create sheet.
         sheet1 = wb.add_sheet('Sheet 1')
         for i in range(len(self.entriesU)):
-            print(i)
+            self.frame8.update_idletasks()
+            self.pb1['value'] += 200/len(self.entriesU)
+            sleep(0.4)
             if i%2 == 1:
                 colName = str(self.entriesU[i])
                 importCol = int(self.entriesU[i-1])
                 sheet1.write(0, i//2, colName)
                 for m in range(1, externalS.nrows):
                     sheet1.write(m, i//2, externalS.cell_value(m, importCol) )
-                    print('saved in col '+str(i))
-        #newFile = input('new file name: ')
-        #wb.save(f'{newFile}.xls')
-        wb.save('new.xls')
-
-        self.sheetnumber_btn = ttk.Button(self.frame5, text="submit number", command = self.new_sheet)
-        self.sheetnumber_btn.pack()
-        self.main_btn = ttk.Button(self.frame5, text="Back to Main", command=self.main)
+        wb.save(f'{self.submitU}.xls')
+        self.main_btn = ttk.Button(self.frame8, text="Done!", command=self.endTask)
         self.main_btn.pack()
+    def endTask(self):
+        for i in self.master.winfo_children():
+            i.destroy()
+        self.frame7 = Frame(self.master, width=300, height=300)
+        self.frame7.pack()
+        self.reg_txt = ttk.Label(self.frame7, text=f'Saved as: {self.submitU}.xls')
+        self.reg_txt.pack()
+        self.reg_txt2 = ttk.Label(self.frame7, text=f'Check your folder')
+        self.reg_txt2.pack()
+        self.reg_txt3 = ttk.Label(self.frame7, text=f'Thanks for using this app')
+        self.reg_txt3.pack()
+        self.get_sheet_btn = ttk.Button(self.frame7, text="Back to Main", command=self.main)
+        self.get_sheet_btn.pack()
+        self.quit_btn = ttk.Button(self.frame7, text="Quit", command=self.master.destroy)
+        self.quit_btn.pack()
 
 def show_cols(loc, index):
     global externalS
@@ -176,33 +202,6 @@ def show_cols(loc, index):
     for i in range(sheet.ncols):
         strin += str(i)+"-  "+sheet.cell_value(0, i) + '\n'
     return strin
-
-def newFile(loc):
-    wb = xlrd.open_workbook(loc)
-    n = 0
-    for i in wb.sheet_names():
-        print (str(n)+'-  '+ i)
-        n+=1
-    sheetnumber = int(input('sheet number: '))
-    sheet = wb.sheet_by_index(sheetnumber)
-
-    for i in range(sheet.ncols):
-        print (str(i)+"-  "+sheet.cell_value(0, i))
-    col = int(input('Eneter number of columnss: '))
-    wb = Workbook()
-    # add_sheet is used to create sheet.
-    sheet1 = wb.add_sheet('Sheet 1')
-
-    for i in range(col):
-        importCol = int(input('select column inport from original: '))
-        colName = input('column name: ')
-        sheet1.write(0, i, colName)
-        for m in range(1, sheet.nrows):
-            sheet1.write(m, i, sheet.cell_value(m, importCol) )
-    newFile = input('new file name: ')
-    wb.save(f'{newFile}.xls')
-
-
 
 root = Tk()
 app(root)
